305911fb70cd47787e0ef8ec70337fc74c03f28e
Ujjawal Kumar <ujjawalbarnbal123@gmail.com>
2026-08-20 02:58:13 +0530

    Add message model, routes, and auth middleware

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -76,3 +76,12 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import http from "http";
 import { Server } from "socket.io";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Basic test route
 app.get("/", (req, res) => {
